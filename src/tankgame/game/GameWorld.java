b50f1d6e46695ea4ad9c88d3a16a21a041bf155a
Abhiram Rishi Prattipati <pabhiramrishi@gmail.com>
2023-05-17 12:54:45 -0500

    added tank2

--- src/tankgame/game/GameWorld.java
+++ src/tankgame/game/GameWorld.java
@@ -17,6 +17,7 @@ public class GameWorld extends JPanel implements Runnable {
     private BufferedImage world;
 
     private Tank t1;
+    private Tank t2;
     private Launcher lf;
     private long tick = 0;
 
@@ -30,15 +31,21 @@ public class GameWorld extends JPanel implements Runnable {
             this.resetGame();
             while (true) {
                 this.tick++;
+
                 this.t1.update();
+                this.t2.update();
+
                 this.repaint();
 
                 Thread.sleep( 1000 / 144);
 
+
+                /**
                 if (this.tick >= 144 * 8) {
                     this.lf.setFrame("end");
                     return;
                 }
+                 */
             }
         } catch (InterruptedException ignored) {
             System.out.println(ignored);
@@ -50,6 +57,8 @@ public class GameWorld extends JPanel implements Runnable {
         this.tick = 0;
         this.t1.setX(300);
         this.t1.setY(300);
+        this.t2.setX(900);
+        this.t2.setY(900);
     }
 
     public void InitializeGame() {
@@ -58,6 +67,8 @@ public class GameWorld extends JPanel implements Runnable {
                 BufferedImage.TYPE_INT_RGB);
 
         BufferedImage t1img = null;
+        BufferedImage t2img = null;
+
         try {
             t1img = ImageIO.read(
                     Objects.requireNonNull(GameWorld.class.getClassLoader().getResource("resources/images/tank1.png"),
@@ -71,6 +82,20 @@ public class GameWorld extends JPanel implements Runnable {
         t1 = new Tank(300, 300, 0, 0, (short) 0, t1img);
         TankControl tc1 = new TankControl(t1, KeyEvent.VK_W, KeyEvent.VK_S, KeyEvent.VK_A, KeyEvent.VK_D, KeyEvent.VK_SPACE);
         this.lf.getJf().addKeyListener(tc1);
+
+        try {
+            t2img = ImageIO.read(
+                    Objects.requireNonNull(GameWorld.class.getClassLoader().getResource("resources/images/tank2.png"),
+                            "Could not find tank2.png")
+            );
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+
+        t2 = new Tank(900, 900, 0, 0, (short) 0, t2img);
+        TankControl tc2 = new TankControl(t2, KeyEvent.VK_UP, KeyEvent.VK_DOWN, KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT, KeyEvent.VK_BACK_SPACE);
+        this.lf.getJf().addKeyListener(tc2);
     }
 
     @Override
@@ -78,6 +103,10 @@ public class GameWorld extends JPanel implements Runnable {
         Graphics2D g2 = (Graphics2D) g;
         Graphics2D buffer = world.createGraphics();
         this.t1.drawImage(buffer);
+        this.t2.drawImage(buffer);
+
+        // this.world.getSubimage();
+
         g2.drawImage(world, 0, 0, null);
     }
 }
